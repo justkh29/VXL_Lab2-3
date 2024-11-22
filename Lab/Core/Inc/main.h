a6af777e04e7d334cf9e07225f201a3c8d1adde5
Hung Ta <khanghung711@gmail.com>
2024-11-22 20:14:08 +0700

    Demo UART

--- Lab/Core/Inc/main.h
+++ Lab/Core/Inc/main.h
@@ -57,60 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_1
-#define B1_GPIO_Port GPIOA
-#define B2_Pin GPIO_PIN_2
-#define B2_GPIO_Port GPIOA
-#define B3_Pin GPIO_PIN_3
-#define B3_GPIO_Port GPIOA
-#define LED_B_Pin GPIO_PIN_4
-#define LED_B_GPIO_Port GPIOA
-#define LED_T_Pin GPIO_PIN_5
-#define LED_T_GPIO_Port GPIOA
-#define LR_Pin GPIO_PIN_6
-#define LR_GPIO_Port GPIOA
-#define LY_Pin GPIO_PIN_7
-#define LY_GPIO_Port GPIOA
-#define SEG1_1_Pin GPIO_PIN_0
-#define SEG1_1_GPIO_Port GPIOB
-#define SEG1_2_Pin GPIO_PIN_1
-#define SEG1_2_GPIO_Port GPIOB
-#define SEG1_3_Pin GPIO_PIN_2
-#define SEG1_3_GPIO_Port GPIOB
-#define SEG2_4_Pin GPIO_PIN_10
-#define SEG2_4_GPIO_Port GPIOB
-#define SEG2_5_Pin GPIO_PIN_11
-#define SEG2_5_GPIO_Port GPIOB
-#define SEG2_6_Pin GPIO_PIN_12
-#define SEG2_6_GPIO_Port GPIOB
-#define SEG2_7_Pin GPIO_PIN_13
-#define SEG2_7_GPIO_Port GPIOB
-#define LG_Pin GPIO_PIN_8
-#define LG_GPIO_Port GPIOA
-#define LR1_Pin GPIO_PIN_9
-#define LR1_GPIO_Port GPIOA
-#define LY1_Pin GPIO_PIN_10
-#define LY1_GPIO_Port GPIOA
-#define LG1_Pin GPIO_PIN_11
-#define LG1_GPIO_Port GPIOA
-#define EN1_Pin GPIO_PIN_12
-#define EN1_GPIO_Port GPIOA
-#define EN2_Pin GPIO_PIN_13
-#define EN2_GPIO_Port GPIOA
-#define SEG1_4_Pin GPIO_PIN_3
-#define SEG1_4_GPIO_Port GPIOB
-#define SEG1_5_Pin GPIO_PIN_4
-#define SEG1_5_GPIO_Port GPIOB
-#define SEG1_6_Pin GPIO_PIN_5
-#define SEG1_6_GPIO_Port GPIOB
-#define SEG1_7_Pin GPIO_PIN_6
-#define SEG1_7_GPIO_Port GPIOB
-#define SEG2_1_Pin GPIO_PIN_7
-#define SEG2_1_GPIO_Port GPIOB
-#define SEG2_2_Pin GPIO_PIN_8
-#define SEG2_2_GPIO_Port GPIOB
-#define SEG2_3_Pin GPIO_PIN_9
-#define SEG2_3_GPIO_Port GPIOB
+#define LED_RED_Pin GPIO_PIN_5
+#define LED_RED_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Lab/Core/Src/main.c
+++ Lab/Core/Src/main.c
@@ -22,14 +22,6 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <software_timer.h>
-#include <button.h>
-#include <light_traffic.h>
-#include <fsm_auto.h>
-#include <fsm_setting.h>
-#include <7SegLED.h>
-#include <fsm_edit.h>
-#include <global_var.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,21 +39,34 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-TIM_HandleTypeDef htim2;
+ADC_HandleTypeDef hadc1;
+
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+uint32_t ADC_value = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_TIM2_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint8_t temp = 0;
+void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart)
+{
+	if (huart->Instance == USART2)
+	{
+		HAL_UART_Transmit(&huart2, &temp, 1, 50);
+		HAL_UART_Receive_IT(&huart2, &temp, 1);
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,9 +97,10 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM2_Init();
+  MX_ADC1_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim2);
+  HAL_UART_Receive_IT(&huart2, &temp, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -105,9 +111,10 @@ int main(void)
 
   while (1)
   {
-	  fsm_auto_run();
-	  fsm_edit();
-	  fsm_config();
+	  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+	  ADC_value = HAL_ADC_GetValue(&hadc1);
+	  HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "%d\n", ADC_value), 1000);
+	  HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -123,6 +130,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -148,50 +156,89 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
-  * @brief TIM2 Initialization Function
+  * @brief ADC1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_TIM2_Init(void)
+static void MX_ADC1_Init(void)
 {
 
-  /* USER CODE BEGIN TIM2_Init 0 */
+  /* USER CODE BEGIN ADC1_Init 0 */
 
-  /* USER CODE END TIM2_Init 0 */
+  /* USER CODE END ADC1_Init 0 */
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE BEGIN TIM2_Init 1 */
+  /* USER CODE BEGIN ADC1_Init 1 */
 
-  /* USER CODE END TIM2_Init 1 */
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 7999;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 9;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  /* USER CODE END ADC1_Init 1 */
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
   }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM2_Init 2 */
+  /* USER CODE BEGIN ADC1_Init 2 */
 
-  /* USER CODE END TIM2_Init 2 */
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 9600;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
 
@@ -206,57 +253,20 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_B_Pin|LED_T_Pin|LR_Pin|LY_Pin
-                          |LG_Pin|LR1_Pin|LY1_Pin|LG1_Pin
-                          |EN1_Pin|EN2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, SEG1_1_Pin|SEG1_2_Pin|SEG1_3_Pin|SEG2_4_Pin
-                          |SEG2_5_Pin|SEG2_6_Pin|SEG2_7_Pin|SEG1_4_Pin
-                          |SEG1_5_Pin|SEG1_6_Pin|SEG1_7_Pin|SEG2_1_Pin
-                          |SEG2_2_Pin|SEG2_3_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : B1_Pin B2_Pin B3_Pin */
-  GPIO_InitStruct.Pin = B1_Pin|B2_Pin|B3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LED_B_Pin LED_T_Pin LR_Pin LY_Pin
-                           LG_Pin LR1_Pin LY1_Pin LG1_Pin
-                           EN1_Pin EN2_Pin */
-  GPIO_InitStruct.Pin = LED_B_Pin|LED_T_Pin|LR_Pin|LY_Pin
-                          |LG_Pin|LR1_Pin|LY1_Pin|LG1_Pin
-                          |EN1_Pin|EN2_Pin;
+  /*Configure GPIO pin : LED_RED_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : SEG1_1_Pin SEG1_2_Pin SEG1_3_Pin SEG2_4_Pin
-                           SEG2_5_Pin SEG2_6_Pin SEG2_7_Pin SEG1_4_Pin
-                           SEG1_5_Pin SEG1_6_Pin SEG1_7_Pin SEG2_1_Pin
-                           SEG2_2_Pin SEG2_3_Pin */
-  GPIO_InitStruct.Pin = SEG1_1_Pin|SEG1_2_Pin|SEG1_3_Pin|SEG2_4_Pin
-                          |SEG2_5_Pin|SEG2_6_Pin|SEG2_7_Pin|SEG1_4_Pin
-                          |SEG1_5_Pin|SEG1_6_Pin|SEG1_7_Pin|SEG2_1_Pin
-                          |SEG2_2_Pin|SEG2_3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
 
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
-{
-	timerRun();
-	getKeyInput();
-}
 /* USER CODE END 4 */
 
 /**
